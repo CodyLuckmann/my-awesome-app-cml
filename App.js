57add4df576a77fbde51e249aad2217d23511593
CodyLuckmann <cluckmann@live.com>
2022-02-01 11:55:57 -0600

    added test files

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ const app = express()
 // Simple Hello World Node App
 const APP_PORT = process.env.APP_PORT || 3000; app.get('/', (req, res) => {
 
-    res.send('Hello to Cody and the rest of the world out there!')
+    res.send('<h2>Hello to Cody and the rest of the world out there!<h2>')
 
 }); app.listen(APP_PORT, () => {
     console.log(`Running app at port:${APP_PORT}`)
---
+++ src/App.test.js
@@ -0,0 +1,15 @@
+const fs = require('fs');
+
+const path = require('path');
+ 
+const html = fs.readFileSync(path.resolve(__dirname, '../App.js'), 'utf8');
+ 
+document.documentElement.innerHTML = html.toString();
+
+test('hello world text', () => {
+ 
+    const linkElement = document.querySelector('h2');
+ 
+    expect(linkElement.textContent).toBe('Hello to Cody and the rest of the world out there!');
+
+});
